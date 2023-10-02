c5b0f33693be13cc838fffd015f96d99c13a73a6
Muhammad Hussain <Muhammadhussainsiddqi@gmail.com>
2023-10-02 17:52:02 +0500

    Add files via upload

---
+++ src/item.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+
+const Item = (prop) => {
+  const [visible, setEditMode] = useState(true);
+  const [content, setcontent] = useState("");
+  const { todo, id, removeTodo, editSavebtn } = prop;
+  
+
+  const handleSavebtn = (id, content) => {
+    setEditMode(true);
+    editSavebtn(id, content);
+  };
+
+  return (
+    <div className="item" id={id}>
+      {visible == true ? (
+        <div>
+          <label htmlFor="checkbox">
+            <input name="checkbox" type="checkbox" />
+          </label>
+          <span>{todo}</span>
+          <button
+            onClick={() => {
+              removeTodo(id);
+            }}
+            name="delete"
+          >
+            Delete
+          </button>
+          <button
+            onClick={() => {
+              setEditMode(false);
+            }}
+            name="edit"
+          >
+            Edit
+          </button>
+        </div>
+      ) : (
+        <div>
+          <input
+            type="text"
+            onChange={(e) => {
+              setcontent(e.target.value);
+            }}
+            value={content}
+          />
+          <button
+            onClick={() => {
+              handleSavebtn(id, content);
+            }}
+          >
+            Save
+          </button>
+        </div>
+      )}
+      <br />
+    </div>
+  );
+};
+
+export default Item;
