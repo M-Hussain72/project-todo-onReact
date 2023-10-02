c884cf4dbff0a27f2bf8d2a0422dd39e3b4893d7
Muhammad Hussain <Muhammadhussainsiddqi@gmail.com>
2023-10-02 17:49:52 +0500

    Add files via upload
    update this file

--- src/todo.jsx
+++ src/todo.jsx
@@ -1,38 +1,36 @@
-import Item from "./items";
+import Item from "./item";
+import TodoAddBtn from "./todoAddBtn"
 import { useState } from "react";
 import uniqid from "uniqid";
 
 const Todo = () => {
-  const [input, setinput] = useState("");
   const [list, setlist] = useState([]);
 
-  const handleChange = (e) => {
-    setinput(e.target.value);
-  };
+  
   const removeTodo = (todoid) => {
-    const newlist = list.filter((item) => item[1] != todoid);
+    const newlist = list.filter((item) => item.id != todoid);
     setlist(newlist);
   };
 
   const editSavebtn = (todoid, content) => {
     let newlist = [...list];
     newlist.map((item) => {
-      if (item[1] === todoid) {
-        item[0] = content;
+      if (item.id === todoid) {
+        item.content = content;
       }
     });
     setlist(newlist);
   };
 
-  const addItems = () => {
+  const addItems = (todoContent) => {
     const arr = [...list];
-    if (input.length > 0) {
+    if (todoContent.length > 0) {
       const id = uniqid();
-      arr.push([input, id]);
+      arr.push({id:id,content:todoContent});
     } else alert("ToDO is empty!");
 
     setlist(arr);
-    setinput("");
+  
   };
 
   return (
@@ -44,8 +42,8 @@ const Todo = () => {
           list.map((item, index) => (
             <Item
               key={index}
-              todo={item[0]}
-              id={item[1]}
+              todo={item.content}
+              id={item.id}
               removeTodo={removeTodo}
               editSavebtn={editSavebtn}
             />
@@ -53,15 +51,7 @@ const Todo = () => {
         ) : (
           <div className="emptyToDo">No ToDo Here</div>
         )}
-        <label htmlFor="text">
-          <input
-            onChange={handleChange}
-            type="text"
-            placeholder="write ToDo"
-            value={input}
-          />
-        </label>
-        <button onClick={addItems}>Add ToDo</button>
+        <TodoAddBtn addItems={addItems}/>
       </from>
     </div>
   );
